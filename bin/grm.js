bee22d480fca26a41a6aed66c741bd96e8d8f343
justinhelmer <jhelmer25@gmail.com>
2016-02-09 15:53:18 -0800

    Bumping to version 0.8.2

--- bin/grm.js
+++ bin/grm.js
@@ -3,7 +3,7 @@
   'use strict';
 
   require('commander')
-      .version('0.8.1')
+      .version('0.8.2')
       .description('Description:\n\n    Download releases, generate documentation, build website, deploy, relax.\n    ' +
           'Type \'help [cmd]\' to see the options for a particlar [command].')
       .command('download', 'download recent releases via the GitHub Tags API')
