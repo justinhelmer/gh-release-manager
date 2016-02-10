370cfaf1ae192f21c46f2320287e45b12fc10178
justinhelmer <jhelmer25@gmail.com>
2016-02-10 10:43:50 -0800

    test moving configs

--- bin/grm.js
+++ bin/grm.js
@@ -3,7 +3,7 @@
   'use strict';
 
   require('commander')
-      .version('0.11.3')
+      .version('0.11.4')
       .description('Description:\n\n    Download releases, generate documentation, build website, deploy, relax.\n    ' +
           'Type \'help [cmd]\' to see the options for a particlar [command].')
       .command('download', 'download recent releases via the GitHub Tags API')
