04b4a39e6020c57401d36bebfb1e0ba228c0842b
justinhelmer <jhelmer25@gmail.com>
2016-02-13 10:48:45 -0800

    Bumping to version 0.12.7

--- bin/grm.js
+++ bin/grm.js
@@ -3,7 +3,7 @@
   'use strict';
 
   require('commander')
-      .version('0.12.6')
+      .version('0.12.7')
       .description('Description:\n\n    Download releases, generate documentation, build website, deploy, relax.\n    ' +
           'Type \'help [cmd]\' to see the options for a particlar [command].')
       .command('download', 'download recent releases via the GitHub Tags API')
